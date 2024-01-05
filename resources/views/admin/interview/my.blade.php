8209d10f58a6705e27a6d621ae321b636747980f
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2024-01-05 15:36:06 +0700

    feat: add my interview schedule page

--- resources/views/admin/interview/my.blade.php
+++ resources/views/admin/interview/my.blade.php
@@ -4,6 +4,36 @@
     <h1 class="text-center text-4xl uppercase font-bold">My Interview Schedule</h1>
 </div>
 <div class="flex flex-col w-full py-8 rounded-lg shadow-xl items-center justify-center mb-8">
+    <div class="px-8 w-full mb-3">
+        <div class="relative mb-4 flex w-full flex-wrap items-stretch">
+        <input
+            id="advanced-search-input"
+            type="search"
+            class="relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+            placeholder="Search"
+            aria-label="Search"
+            aria-describedby="button-addon1" />
+    
+        <!--Search button-->
+        <button
+            class="relative z-[2] flex items-center rounded-r bg-primary px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
+            type="button"
+            id="advanced-search-button"
+            data-te-ripple-init
+            data-te-ripple-color="light">
+            <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            class="h-5 w-5">
+            <path
+                fill-rule="evenodd"
+                d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                clip-rule="evenodd" />
+            </svg>
+        </button>
+        </div>
+    </div>
     <div id="datatable" class="w-full px-5 py-5"></div>
 </div>
 @endSection('content')
@@ -11,27 +41,8 @@
     <script>
         const customDatatable = document.getElementById("datatable");
         const data = JSON.parse(@json($interview));
-        console.log(typeof(data));
-        console.log(data);
-        const setActions = () => {
-        document.querySelectorAll(".call-btn").forEach((btn) => {
-            btn.addEventListener("click", () => {
-            console.log(`call ${btn.attributes["data-te-number"].value}`);
-            });
-        });
 
-        document.querySelectorAll(".message-btn").forEach((btn) => {
-            btn.addEventListener("click", () => {
-            console.log(
-                `send a message to ${btn.attributes["data-te-email"].value}`
-            );
-            });
-        });
-        };
-
-        customDatatable.addEventListener("render.te.datatable", setActions);
-
-        new te.Datatable(
+        const instance = new te.Datatable(
         customDatatable,
         {
             columns: [
@@ -82,5 +93,28 @@
         },
         { hover: true }
         );
+        const advancedSearchInput = document.getElementById('advanced-search-input');
+
+        const search = (value) => {
+            let [phrase, columns] = value.split(" in:").map((str) => str.trim());
+
+            if (columns) {
+            columns = columns.split(",").map((str) => str.toLowerCase().trim());
+            }
+
+            instance.search(phrase, columns);
+        };
+
+        document
+            .getElementById("advanced-search-button")
+            .addEventListener("click", (e) => {
+            search(advancedSearchInput.value);
+            });
+
+        advancedSearchInput.addEventListener("keydown", (e) => {
+            if (e.keyCode === 13) {
+            search(e.target.value);
+            }
+        });
     </script>
 @endSection('script')
