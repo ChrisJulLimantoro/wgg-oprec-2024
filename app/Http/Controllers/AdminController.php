f20610a5a71da1a07b1077b7d57cc6281efeef67
Darrell Cornelius Rivaldo <darrellcornelius37@gmail.com>
2024-01-09 22:04:36 +0700

    fix: applicant relation in cv & division not showing in uploaded application form

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -25,7 +25,7 @@ class AdminController extends BaseController
         if ($applicant->stage < 2) {
             return 'Pendaftar masih belum mengupload berkas';
         }
-
+        $applicant->load($applicant->relations());
         $cv = $applicant->cv();
         return $cv->stream('CV_' . $applicant->getNRP() . '.pdf');
     }
