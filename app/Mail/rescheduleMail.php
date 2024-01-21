490dcfb64180406c62af1b3e1786b4c5cae17d0c
Nico <nicos.t.65@gmail.com>
2024-01-21 20:32:49 +0700

    chore: implement ShouldQueue

--- app/Mail/rescheduleMail.php
+++ app/Mail/rescheduleMail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class rescheduleMail extends Mailable
+class rescheduleMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
