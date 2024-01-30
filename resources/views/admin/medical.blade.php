d27604141b719461d12a5e160520d0b653320638
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2024-01-30 21:43:39 +0700

    Fix: medical form view

--- resources/views/admin/medical.blade.php
+++ resources/views/admin/medical.blade.php
@@ -63,7 +63,7 @@
                     id="exampleFormControlTextarea13" rows="2" placeholder="" name="medical_history[disease_explanation]">{{ old('medical_history.disease_explanation') ?? (data_get($admin, 'medical_history.disease_explanation') ?? '') }}</textarea>
                 <label for="exampleadminControlTextarea13"
                     class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">
-                    Disease Explantion
+                    Disease Explanation
                 </label>
             </div>
         </div>
