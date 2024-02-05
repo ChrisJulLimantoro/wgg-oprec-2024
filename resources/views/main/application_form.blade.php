023fa116f72e84b96e31d711559c25c49bbb5adb
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2024-02-05 20:35:45 +0700

    fix: add deskripsi domisili

--- resources/views/main/application_form.blade.php
+++ resources/views/main/application_form.blade.php
@@ -171,7 +171,7 @@
                             id="exampleInput123" aria-describedby="emailHelp123" placeholder="Provinsi" name="province" />
                         <label for="emailHelp123"
                             class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">
-                            Provinsi
+                            Provinsi (domisili saat ini)
                         </label>
                     </div>
 
@@ -185,7 +185,7 @@
                             name="city" />
                         <label for="emailHelp123"
                             class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">
-                            Kota/Kabupaten
+                            Kota/Kabupaten (domisili saat ini)
                         </label>
                     </div>
                 </div>
@@ -199,7 +199,7 @@
                         id="exampleInput125" placeholder="Alamat" name="address" />
                     <label for="exampleInput125"
                         class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200">
-                        Alamat
+                        Alamat (domisili saat ini)
                     </label>
                 </div>
 
